db8c23c746e057ee1728c4ee82030de8b934966d
spatialos-admin <80092049+rajgohil28@users.noreply.github.com>
2025-04-21 12:30:41 +0530

    Prepare for GitHub Pages deploy

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/3d-meshgen-ai/',
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['@react-three/fiber', 'three', 'react', 'react-dom', '@react-three/drei', 'openai']
+    }
+  }
 })
